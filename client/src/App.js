dec2fccf9921f901e0df32d03ec5fc2d44c9f45b
shubham <shubh.shubham21@gmail.com>
2022-05-30 21:03:35 +0530

    search filter in api

---
+++ client/src/App.js
@@ -0,0 +1,87 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+import { Users } from './users';
+import Table from './Table';
+import axios from 'axios';
+
+////////////////////// BASIC SEARCH ///////////
+
+// function App() {
+// 	const [query, setQuery] = useState('');
+
+// 	return (
+// 		<div className='App'>
+// 			<input
+// 				type='text'
+// 				className='search'
+// 				placeholder='Search'
+// 				onChange={(e) => setQuery(e.target.value)}
+// 			/>
+
+// 			<ul className='list'>
+// 				{Users.filter((user) =>
+// 					user.first_name.toLowerCase().includes(query)
+// 				).map((user) => (
+// 					<li key={user.id} className='listItem'>
+// 						{user.first_name}
+// 					</li>
+// 				))}
+// 			</ul>
+// 		</div>
+// 	);
+// }
+
+///////////////////////SEARCH ON A DATATABLE
+
+// function App() {
+// 	const [query, setQuery] = useState('');
+
+// 	const keys = ['first_name', 'last_name', 'email'];
+
+// 	const search = (data) => {
+// 		return data.filter((item) =>
+// 			keys.some((key) => item[key].toLowerCase().includes(query))
+// 		);
+// 	};
+
+// 	return (
+// 		<div className='App'>
+// 			<input
+// 				className='search'
+// 				placeholder='Search...'
+// 				onChange={(e) => setQuery(e.target.value.toLowerCase())}
+// 			/>
+// 			{<Table data={search(Users)} />}
+// 		</div>
+// 	);
+// }
+
+///////////////////////////////// API SEARCH ////////////////
+
+function App() {
+	const [query, setQuery] = useState('');
+	const [data, setData] = useState([]);
+
+	useEffect(() => {
+		const fetchUsers = async () => {
+			const res = await axios.get(`http://localhost:5000?q=${query}`);
+			setData(res.data);
+		};
+		if (query.length === 0 || query.length > 2) fetchUsers();
+	}, [query]);
+
+	return (
+		<div className='App'>
+			<input
+				type='text'
+				className='search'
+				placeholder='Search'
+				onChange={(e) => setQuery(e.target.value)}
+			/>
+
+			{<Table data={data} />}
+		</div>
+	);
+}
+
+export default App;
